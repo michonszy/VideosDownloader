a7ef905318c060cf33ed8ac9093b0762d3775c08
Szymon Michoń <szymon.michon@outlook.com>
2020-12-08 21:00:32 +0100

    Dodaj pliki projektów.

---
+++ YouTube Downloader/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+using System.Xml.Linq;
+using System.Net;
+using System.IO;
+
+namespace YouTube_Downloader
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Pobieranie(string link, string name)
+        {
+            using(WebClient Client = new WebClient())
+            {
+                Client.DownloadFile(link, name);
+            }
+        }
+
+        public void GetVideo_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text;
+            HttpWebRequest myHttpWebRequest1 = (HttpWebRequest)WebRequest.Create(url);
+            
+            if (url.Contains("youtube"))
+            {
+                string zmieniony = url.Replace(".com/watch?v=", ".com/embed/");
+                
+                zmieniony = zmieniony + "?autoplay=1";
+                webBrowser1.Navigate(zmieniony);
+                textBox1.Text = zmieniony;
+            }
+            else
+            {
+                webBrowser1.Navigate(textBox1.Text);
+            }
+
+
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Pobieranie(textBox1.Text, textBox2.Text);
+                MessageBox.Show("Wszystko OK", "Plik został pobrany");
+                string url = textBox1.Text;
+                if (url.Contains("youtube"))
+                {
+                    url.Replace(".com/watch?v=", ".com/embed/");
+
+                }
+
+            }
+            catch(Exception wyjatek)
+            {
+                MessageBox.Show("Błąd!", "Podany błąd to: " + wyjatek.ToString());
+            }
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
---
+++ YouTube Downloader/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("YouTube Downloader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("YouTube Downloader")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("d1ce906c-8a86-4e03-a3b1-328667f9cb0b")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
